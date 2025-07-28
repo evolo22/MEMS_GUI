5bf0ebc0759614db88e09ba11e1bd195af83a323
Liz Voloshin <evoloshin@ucdavis.edu>
2025-07-28 11:20:58 -0700

    Figuring Out Blueooth

--- app/src/main/java/com/example/mems_gui/MainActivity.kt
+++ app/src/main/java/com/example/mems_gui/MainActivity.kt
@@ -1,42 +1,51 @@
 package com.example.mems_gui
 
+import android.bluetooth.BluetoothManager
 import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.example.mems_gui.ui.theme.MEMS_GUITheme
-import com.google.accompanist.pager.*
-import androidx.compose.ui.tooling.preview.Preview
-
-
-import android.Manifest
-import android.app.Activity
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothSocket
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.util.Log
-import androidx.core.app.ActivityCompat
-import java.io.IOException
-import java.util.*
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.getAdapter()
+
+//        if (bluetoothAdapter == null) {
+//            Log.d("BluetoothCheck", "This device doesn't support Bluetooth")
+//        }
+//        else{
+//            Log.d("BluetoothCheck", "We are good")
+//        }
+
         setContent {
             MEMS_GUITheme {
                 TwoPageSwipeUI()
@@ -59,6 +68,15 @@ fun TwoPageSwipeUI() {
             0 -> ConnectScreen()
             1 -> GraphScreen()
         }
+    }
+}
+
+
+fun Ask_For_Connectviity()
+{
+    if (bluetoothAdapter?.isEnabled == false) {
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
     }
 }
 
